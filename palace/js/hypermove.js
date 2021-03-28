5c096c37b2ee271abe0575f4b549374a7aa7dd39
Mingxuan Xie <31252716+MingxuanXie@users.noreply.github.com>
2021-03-28 19:53:34 +0800

    adjust UI

--- palace/js/hypermove.js
+++ palace/js/hypermove.js
@@ -4,10 +4,30 @@ Make a-object move randomly
 @license  MIT
 */
 
+// let factor;
+// let oldfactor;
+
 AFRAME.registerComponent('hypermove', {
     schema: {
         factor:{type: 'number', default: 0.05},
     },
+    init: function(){
+        var cursorbox = document.getElementById("cursorbox"); // get cursor box to hide
+        var el = this.el;
+        // factor = this.data.factor;
+        // oldfactor = factor;
+        el.addEventListener('raycaster-intersected',function(){
+            // factor = 0;
+            cursorbox.setAttribute('visible', false);
+            this.setAttribute('factor', 0)
+            // console.log("hovered") 
+        });
+        el.addEventListener('raycaster-intersected-cleared',function(){
+            // factor = oldfactor;
+            cursorbox.setAttribute('visible', true);
+            // console.log("leaved") 
+        });
+       },
     tick: function (time, timeDelta) {
         this.el.object3D.position.x += this.data.factor*(Math.random() - 0.5);
         this.el.object3D.position.y += this.data.factor*(Math.random() - 0.5);
